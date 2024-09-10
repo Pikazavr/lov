0c3567c1c41671b18f052383fdcaab552f941897
Kulikov I.A. <pps.22@mail.ru>
2024-09-10 11:17:36 +0300

    task 1 and 2

---
+++ lb 1/lb 1.cpp
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main() {
+    int max = 0;
+    int min = 999;
+    int res = 0;
+    int mas[10];
+       
+    srand(time(NULL)); 
+    for (int i = 0; i < 10; i++) {
+        mas[i] = rand() % 100; 
+        printf("%d ", mas[i]); 
+    }
+    printf("\n");
+
+       for (int i = 0; i < 10; i++) {
+        if (mas[i] > max) {
+            max = mas[i];
+        }
+        if (mas[i] < min) {
+            min = mas[i];
+        }
+    }
+
+        res = max - min;
+    printf("Diff %d \n", res);
+    return 0;
+}
